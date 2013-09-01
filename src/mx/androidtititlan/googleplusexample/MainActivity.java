5395fa289a0700e9c084776ce1840507de0641cb
Enrique Diaz <nrikediaz@androidtitlan.org>
2013-09-01 02:52:00 -0500

    Adding source

---
+++ src/mx/androidtititlan/googleplusexample/MainActivity.java
@@ -0,0 +1,91 @@
+package mx.androidtititlan.googleplusexample;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.IntentSender.SendIntentException;
+import android.os.Bundle;
+import android.view.Menu;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
+import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
+import com.google.android.gms.plus.PlusClient;
+
+public class MainActivity extends Activity implements ConnectionCallbacks,
+		OnConnectionFailedListener {
+	private static final int REQUEST_CODE_RESOLVE_ERR = 9000;
+	private ProgressDialog mConnectionProgressDialog;
+	private PlusClient mPlusClient;
+	private ConnectionResult mConnectionResult;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		mPlusClient = new PlusClient.Builder(this, this, this)
+				.setVisibleActivities("http://schemas.google.com/AddActivity",
+						"http://schemas.google.com/BuyActivity").build();
+		mConnectionProgressDialog = new ProgressDialog(this);
+		mConnectionProgressDialog.setMessage("Signin in...");
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult result) {
+		if (mConnectionProgressDialog.isShowing()) {
+			if (result.hasResolution()) {
+				try {
+					result.startResolutionForResult(this,
+							REQUEST_CODE_RESOLVE_ERR);
+				} catch (SendIntentException exception) {
+					mPlusClient.connect();
+
+				}
+
+			}
+		}
+		mConnectionResult = result;
+	}
+
+	@Override
+	public void onConnected(Bundle arg0) {
+		// No problem
+		mConnectionProgressDialog.dismiss();
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int responseCode,
+			Intent data) {
+		if (requestCode == REQUEST_CODE_RESOLVE_ERR
+				&& responseCode == RESULT_OK) {
+			mConnectionResult = null;
+			mPlusClient.connect();
+		}
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		mPlusClient.connect();
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		mPlusClient.disconnect();
+	}
+
+	@Override
+	public void onDisconnected() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
